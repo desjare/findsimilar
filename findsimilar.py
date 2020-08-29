4adc0f21a6a33467952f45bb4dc4569760aac4db
Eric Desjardins <desjare@gmail.com>
2020-08-29 09:48:01 -0400

    Add md5 comparison.

--- findsimilar.py
+++ findsimilar.py
@@ -9,6 +9,8 @@ import difflib
 if sys.version_info[0] < 3:
 	raise Exception("Must be using Python 3")
 
+default_ext = [".mp4", ".mkv", ".m4v", ".avi", ".jpg", ".jpeg"]
+
 def list_files_in_folder(folder, exts, verbose):
 	l = []
 	for root, subdirs, files in os.walk(folder):
@@ -32,20 +34,42 @@ def get_filename_without_ext(filename):
 	name, ext = os.path.splitext(filename)
 	return os.path.basename(name)
 
-def find_similar(files, min_ratio, verbose):
+def hash_file(path, blocksize=65536):
+    afile = open(path, 'rb')
+    hasher = hashlib.md5()
+    buf = afile.read(blocksize)
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = afile.read(blocksize)
+    afile.close()
+    return hasher.hexdigest()
+
+def find_similar(files, min_ratio, compare, verbose):
 	similar_tuple_list = []
 	similar_set = set()
 
 	for f1 in files:
 		for f2 in files:
-			if f1 != f2 and not f1+f2 in similar_set:
+			name1, ext1 = os.path.splitext(f1)
+			name2, ext2 = os.path.splitext(f2)
+
+			if f1 != f2 and ext1.lower() == ext2.lower() and not f1+f2 in similar_set:
 				ratio = is_similar(get_filename_without_ext(f1),get_filename_without_ext(f2))
 				if ratio >= min_ratio:
 					if verbose is True:
 						print("Similar %s <-> %s ratio:%f" % (os.path.basename(f1), os.path.basename(f2), ratio))
 
+					same = False
+
+					if compare is True and ratio == 1.0:
+						hash1 = hash_file(f1)
+						hash2 = hash_file(f2)
+
+						if hash1 == hash2:
+							same = True
+
 					similar_set.add(f1+f2)
-					similar_tuple_list.append((ratio, f1, f2))
+					similar_tuple_list.append((ratio, f1, f2, same))
 
 	return similar_tuple_list
 
@@ -53,8 +77,9 @@ if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser(description="findsimilar")
 	parser.add_argument("--folders", nargs="+", action="store", dest="folders", default=["."], help="directoris to scan", required=True)
-	parser.add_argument("--exts", nargs="+", action="store", dest="exts", default=[".mp4", ".mkv", ".m4v", ".avi"], help="extensions to scan", required=False)
+	parser.add_argument("--exts", nargs="+", action="store", dest="exts", default=default_ext, help="extensions to scan", required=False)
 	parser.add_argument("--verbose", action="store_true", dest="verbose", default=False, help="verbose", required=False)
+	parser.add_argument("--compare", action="store_true", dest="compare", default=False, help="compare similar files", required=False)
 	parser.add_argument("--minratio", action="store", type=float, dest="min_ratio", default=0.9, help="minimum matching ratio", required=False)
 	args = parser.parse_args()
 	files = []
@@ -71,12 +96,12 @@ if __name__ == '__main__':
 			print('%s is not a valid path, please verify' % (folder))
 			sys.exit(1)
 
-	similar_tuple_list = find_similar(files, args.min_ratio, args.verbose)
+	similar_tuple_list = find_similar(files, args.min_ratio, args.compare, args.verbose)
 
 	for similar in similar_tuple_list:
-		ratio, f1, f2 = similar
+		strings = [str(item) for item in similar]
+		print(";".join(strings))
 
-		print("Found ratio %f\n%s\n%s\n" % (ratio, f1, f2))
 
 
 
